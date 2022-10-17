f5ac146c80f306e0c02534ea53b84daf9b7251ba
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-17 13:39:19 -0500

    Including new actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  post "/articles", to: "articles#create"
+  get "/articles/new", to: "articles#new"
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show", as: :article
-  root "articles#index"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
