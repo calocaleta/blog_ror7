96342426320d86bbcc6064f7c9f2f8b3f352fc97
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-17 22:27:24 -0500

    Adding delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,11 @@ class ArticlesController < ApplicationController
             render :edit
         end
     end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
 
     private
     def articles_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
+  delete "/articles/:id", to: "articles#destroy"
   patch "/articles/:id", to: "articles#update"
   get "/articles/:id/edit", to: "articles#edit"
   post "/articles", to: "articles#create"
